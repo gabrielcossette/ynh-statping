8bedec150810e89461b34c2c3d6f8962b9c93d74
Gabriel Cossette <gabriel@gcossette.com>
2020-04-18 17:42:47 -0400

    force docker image

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -2,11 +2,13 @@
 
 # example :
 #
-[ "$architecture" == "amd64" ] && image=statping/statping
-[ "$architecture" == "i386" ]  && image=statping/statping
+#[ "$architecture" == "amd64" ] && image=statping/statping
+#[ "$architecture" == "i386" ]  && image=statping/statping
 #[ "$architecture" == "armhf" ] && image=portainer/portainer:linux-arm-1.14.0
-[ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
+#[ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
 #
+image=statping/statping
+
 options="-p $port:8080 -v $data_path/data:/app"
 #
 docker run -d --name=$app --restart always $options $image 1>&2
