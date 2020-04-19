27aaa94051d449208edd82c4ecb614709e44a0b0
Gabriel Cossette <gabriel@gcossette.com>
2020-04-18 21:05:03 -0400

    fix

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -8,7 +8,7 @@
 #[ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
 #
 image=statping/statping
-options="-p $port:8080 -v $data_path/data:/app -e \"host=$(ip r s 0/0 | awk '{print $3}')\""
+options="-p $port:8080 -v $data_path/data:/app -e host=$(ip r s 0/0 | awk '{print $3}')"
 
 docker run -d --name=$app --restart always $options $image
 #echo $?
